174a7980baba2895b3024a8323f79b1d2401bd92
Tom Parker <palfrey@tevp.net>
2016-03-14 17:38:59 +0000

    Cope with no old permissions file

--- compare-permissions.py
+++ compare-permissions.py
@@ -14,37 +14,41 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 yaml_path = path.join(config["folder"], config["repos"])
+if not path.exists(yaml_path):
+	raise Exception, "Can't find %s. Did you run list-repos.py first?" % yaml_path
 current_repos = yaml.load(open(yaml_path))
-previous_repos = yaml.load(open(yaml_path + ".old"))
-
-def makePermsDict(repo):
-	return {x["who"]: (x["what"],x["why"]) for x in repo["access"]}
 
 changes = []
 
-def note_change(msg):
-	changes.append(msg)
-	logging.warning(msg)
+if path.exists(yaml_path + ".old"): # Otherwise, changes is empty
+	previous_repos = yaml.load(open(yaml_path + ".old"))
 
-for r in current_repos:
-	if r == "_when": # Magic entry that has info on when this was recorded
-		continue
-	logging.debug("Checking %s", r)
-	repo = makePermsDict(current_repos[r])
-	old_repo = makePermsDict(previous_repos[r])
-	for k in repo:
-		user = repo[k]
-		if k not in old_repo:
-			note_change("%s has been added to %s with permissions %s because of %s"% (k, r, user[0], user[1]))
-		else:
-			old_user = old_repo[k]
-			if old_user[0] != user[0]:
-				note_change("%s has changed permissions from %s to %s because of %s on %s (was %s)"% (k, old_user[0], user[0], user[1], r, old_user[1]))
+	def makePermsDict(repo):
+		return {x["who"]: (x["what"],x["why"]) for x in repo["access"]}
 
-	for k in old_repo:
-		user = old_repo[k]
-		if k not in repo:
-			note_change("%s has been removed %s from permissions %s. Old reason was %s"% (k, r, user[0], user[1]))
+	def note_change(msg):
+		changes.append(msg)
+		logging.warning(msg)
+
+	for r in current_repos:
+		if r == "_when": # Magic entry that has info on when this was recorded
+			continue
+		logging.debug("Checking %s", r)
+		repo = makePermsDict(current_repos[r])
+		old_repo = makePermsDict(previous_repos[r])
+		for k in repo:
+			user = repo[k]
+			if k not in old_repo:
+				note_change("%s has been added to %s with permissions %s because of %s"% (k, r, user[0], user[1]))
+			else:
+				old_user = old_repo[k]
+				if old_user[0] != user[0]:
+					note_change("%s has changed permissions from %s to %s because of %s on %s (was %s)"% (k, old_user[0], user[0], user[1], r, old_user[1]))
+
+		for k in old_repo:
+			user = old_repo[k]
+			if k not in repo:
+				note_change("%s has been removed %s from permissions %s. Old reason was %s"% (k, r, user[0], user[1]))
 
 msg = MIMEMultipart()
 when = datetime.datetime.now().strftime("%Y-%m-%d")
