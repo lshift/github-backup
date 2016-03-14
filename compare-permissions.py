ac713ed2758c921c19769b1df7e5bcdf68064f76
Tom Parker <palfrey@tevp.net>
2016-03-14 17:37:27 +0000

    Email changes

--- compare-permissions.py
+++ compare-permissions.py
@@ -1,6 +1,11 @@
 import yaml
 import os.path as path
 import logging
+import datetime
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 config = yaml.load(open("backup.yaml"))
 logging.basicConfig(
@@ -15,7 +20,12 @@ previous_repos = yaml.load(open(yaml_path + ".old"))
 def makePermsDict(repo):
 	return {x["who"]: (x["what"],x["why"]) for x in repo["access"]}
 
-allsame = True
+changes = []
+
+def note_change(msg):
+	changes.append(msg)
+	logging.warning(msg)
+
 for r in current_repos:
 	if r == "_when": # Magic entry that has info on when this was recorded
 		continue
@@ -25,23 +35,44 @@ for r in current_repos:
 	for k in repo:
 		user = repo[k]
 		if k not in old_repo:
-			allsame = False
-			logging.warning("%s has been added to %s with permissions %s because of %s", k, r, user[0], user[1])
+			note_change("%s has been added to %s with permissions %s because of %s"% (k, r, user[0], user[1]))
 		else:
 			old_user = old_repo[k]
 			if old_user[0] != user[0]:
-				allsame = False
-				logging.warning("%s has changed permissions from %s to %s because of %s on %s (was %s)", k, old_user[0], user[0], user[1], r, old_user[1])
+				note_change("%s has changed permissions from %s to %s because of %s on %s (was %s)"% (k, old_user[0], user[0], user[1], r, old_user[1]))
 
 	for k in old_repo:
 		user = old_repo[k]
 		if k not in repo:
-			allsame = False
-			logging.warning("%s has been removed %s from permissions %s. Old reason was %s", k, r, user[0], user[1])
+			note_change("%s has been removed %s from permissions %s. Old reason was %s"% (k, r, user[0], user[1]))
 
-if not allsame:
+msg = MIMEMultipart()
+when = datetime.datetime.now().strftime("%Y-%m-%d")
+if changes != []:
 	when = previous_repos["_when"]
 	new_path = yaml_path + when.strftime("-%Y-%m-%dT%H:%M:%S%z")
 	logging.warning("Writing to %s because of changes", new_path)
 	with open(new_path, "w") as new_file:
 		yaml.dump(previous_repos, new_file)
+	msg['Subject'] = 'CHANGES: Github permission changes for %s' % when
+	text = "Changes!\n--------\n\n" + "\n".join(changes)
+else:
+	msg['Subject'] = 'Github permission list for %s' % when
+	text = "No changes"
+
+msg['From'] = config["email_from"]
+msg['To'] = config["email_to"]
+changes = MIMEText(text, "plain")
+msg.attach(changes)
+
+with open(yaml_path) as fp:
+	yaml_block = MIMEText(fp.read(), "x-mime")
+	yaml_block.add_header('Content-Disposition', 'attachment', filename="permissions.yaml")
+	msg.attach(yaml_block)
+
+composed = msg.as_string()
+file("dump", "w").write(composed)
+
+s = smtplib.SMTP(config["smtp_server"])
+s.sendmail(config["email_from"], config["email_to"], composed)
+s.quit()
