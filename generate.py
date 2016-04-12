f02705d5616270b62cde8931578906b571dda832
Tom Parker <palfrey@tevp.net>
2016-04-12 11:00:17 +0100

    Fix the path for the old copy of the repos details

--- generate.py
+++ generate.py
@@ -2,11 +2,12 @@ import jinja2
 import yaml
 import os
 import stat
+import os.path as path
 
 loader = jinja2.FileSystemLoader('.')
 env = jinja2.Environment(loader=loader)
 config = yaml.load(open("backup.yaml"))
-
+config["repos_fullpath"] = path.abspath(path.join(config["backup_folder"], config["repos"]))
 template = env.get_template('backup.sh.template')
 template.stream(config).dump("backup.sh")
 os.chmod("backup.sh", os.stat("backup.sh").st_mode | stat.S_IXUSR)
